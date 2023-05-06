7e3e0b063cd3dc9a20d37f78d245ce518572a591
Giorgos Nitsos <nitsosgeorg@gmail.com>
2023-05-06 18:19:10 +0300

    selection screen profile list

--- app/src/main/java/aueb/msc/ProfileSelection.kt
+++ app/src/main/java/aueb/msc/ProfileSelection.kt
@@ -5,21 +5,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import aueb.msc.db.DatabaseHelper
 import aueb.msc.model.Profile
 
 class ProfileSelection : AppCompatActivity() {
 
     private lateinit var databaseHelper: DatabaseHelper
+    private val activity = this@ProfileSelection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_selection)
+        databaseHelper = DatabaseHelper(activity)
+        //Display existing profiles
         val profileNames = databaseHelper.getAllProfileNames()
+        val linearLayout = findViewById<LinearLayout>(R.id.profile_selection_linear_layout)
+        profileNames.forEach { buttonText ->
+            val button = Button(this)
+            button.text = buttonText
+            linearLayout.addView(button)
+        }
+        // Create new profile
         val newProfileButton = findViewById<Button>(R.id.create_new_profile_button)
-        newProfileButton.setOnClickListener(){
+        newProfileButton.setOnClickListener() {
             val intent = Intent(this, VehicleProfileSetup::class.java)
             startActivity(intent)
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
