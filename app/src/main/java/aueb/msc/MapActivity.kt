bdff17908fd240a98184874ae83a10cc64602fc1
Michas78 <62755654+Michas78@users.noreply.github.com>
2023-06-03 12:07:59 +0300

    First push of fragment map

---
+++ app/src/main/java/aueb/msc/MapActivity.kt
@@ -0,0 +1,21 @@
+package aueb.msc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import aueb.msc.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        // Initialize fragment
+        val fragment: Fragment = MapFragment()
+
+        // Open fragment
+        supportFragmentManager
+            .beginTransaction().replace(R.id.frame_layout, fragment)
+            .commit()
+    }
+}
